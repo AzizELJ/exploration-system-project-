beef5e24aaa6c57c867f3ecd113c1788b3523d5b
aziz el janan <abdo6janan@gmail.com>
2024-02-01 21:46:49 +0100

    file services of the exploration system project

---
+++ services/file-services.py
@@ -0,0 +1,9 @@
+class Files_factory :
+    def __init__(self, types_dict : dict) -> None:
+        types_dict = types_dict
+    def addff():
+        pass # besoin de comprendre cette partie et de files factory en general
+    def instantiate():
+        pass # besoin de comprendre cette partie et de files factory en general
+    def list():
+        pass # besoin de comprendre cette partie et de files factory en general
